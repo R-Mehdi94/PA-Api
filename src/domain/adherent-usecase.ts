2cf4304b4d6d7eaeeb0fe279082a9c7469772db8
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-07-28 01:22:50 +0200

    changePassword

--- src/domain/adherent-usecase.ts
+++ src/domain/adherent-usecase.ts
@@ -2,7 +2,6 @@ import { DataSource, DeleteResult } from "typeorm";
 import { Adherent } from "../database/entities/adherent";
 import { User } from "../database/entities/user";
 import { Token } from "../database/entities/token";
-import { compare } from "bcrypt";
 
 export interface ListAdherentRequest {
     page: number
@@ -36,27 +35,17 @@ export interface UpdateAdherentParams {
 export class AdherentUsecase {
     constructor(private readonly db: DataSource) { }
 
-    async verifMdp(id: number, mdp: string): Promise<boolean> {
+    async verifMdp(id:number, mdp: string): Promise<boolean> {
         const entityManager = this.db.getRepository(Adherent);
-    
-        // Récupérer le mot de passe haché de l'utilisateur avec l'ID spécifié
-        const sqlQuery = `SELECT motDePasse FROM adherent WHERE id = ?;`;
-        const result = await entityManager.query(sqlQuery, [id]);
-    
-        if (result.length === 0) {
-            console.log("Utilisateur non trouvé");
-            return false;
-        }
-    
-        const hashedPassword = result[0].motDePasse;
-    
-        const isPasswordValid = await compare(mdp, hashedPassword);
-    
-        if (!isPasswordValid) {
+
+        const sqlQuery = `select count(*) from adherent where motDePasse like ? and id = ?;`;
+
+        const verifVisiteur = await entityManager.query(sqlQuery, [mdp,id]);
+        if(verifVisiteur[0]['count(*)'] === 0){
             console.log("Mot de passe incorrect");
             return false;
         }
-    
+
         return true;
     }
 
