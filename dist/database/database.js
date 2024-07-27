3a5453093e977234c0190ff1e49204ae55665959
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-07-27 14:27:20 +0200

    correction database

--- dist/database/database.js
+++ dist/database/database.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 require("dotenv/config");
-exports.AppDataSource = new typeorm_1.DataSource({
+/*export const AppDataSource = new DataSource({
     type: "mysql",
     host: 'db',
     port: 3306,
@@ -16,32 +16,29 @@ exports.AppDataSource = new typeorm_1.DataSource({
         "src/database/entities/*.ts"
     ]
 });
-/*ssl: {
+/*  ssl: {
     rejectUnauthorized: false // Ajustez selon vos besoins de sécurité
   },
 "driver": require('mysql2')*/
-/*export const AppDataSource = new DataSource({
-      type: process.env.TYPE as any,
-      host: process.env.HOST,
-      port: process.env.PORTDB as any,
-      username: process.env.USERMYSQL,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      logging: true,
-      synchronize: false,
-      entities:[
-          "dist/database/entities/*.js"
-          //"src/database/entities/*.ts"
-  
-      ],
-      migrations:[
-         "dist/database/migrations/*.js"
-         //"src/database/migrations/*.ts"
-  
-      ],
-      ssl: {
-          rejectUnauthorized: false
-        },
-      "driver": require('mysql2')
-  
-  })*/ 
+exports.AppDataSource = new typeorm_1.DataSource({
+    type: process.env.TYPE,
+    host: process.env.HOST,
+    port: process.env.PORTDB,
+    username: process.env.USERMYSQL,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    logging: true,
+    synchronize: false,
+    entities: [
+        "dist/database/entities/*.js"
+        //"src/database/entities/*.ts"
+    ],
+    migrations: [
+        "dist/database/migrations/*.js"
+        //"src/database/migrations/*.ts"
+    ],
+    ssl: {
+        rejectUnauthorized: false
+    },
+    "driver": require('mysql2')
+});
